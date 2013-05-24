5e262740002b4779ca07f3b2e24c65607a392037
Tomasz Przybysz <tpr@touk.pl>
2013-05-24 21:18:35 +0200

    project setup for Spocko Groovy workshop with readme

---
+++ spockogroovy/src/test/groovy/spockogroovy/Chapter1SpockBasicsSpecification.groovy
@@ -0,0 +1,29 @@
+package spockogroovy
+
+import spock.lang.Specification
+
+/*
+Although Spock uses a different terminology, many of its concepts and features are inspired from JUnit.
+
+    Spock                   JUnit
+    -----                   -----
+    Specification           Test class
+    setup()                 @Before
+    cleanup()               @After
+    setupSpec()             @BeforeClass
+    cleanupSpec()           @AfterClass
+    Feature                 Test
+    Parametrized feature    Theory
+    Condition               Assertion
+    Exception condition     @Test(expected=...)
+    @FailsWith              @Test(expected=...)
+    Interaction             Mock expectation (EasyMock, JMock, ...)
+*/
+
+class Chapter1SpockBasicsSpecification extends Specification {
+
+    def 'I am a specification feature'() {
+        expect:
+        2 + 2 == 4
+    }
+}
